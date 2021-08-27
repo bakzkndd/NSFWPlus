2f9544d62f9958769aaf5c4aa6f7037463052093
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-08-27 21:07:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,17 +33,7 @@ module.exports = class BlurNSFW extends Plugin {
 		const channelTextArea = await getModule(m => m.type && m.type.render && m.type.render.displayName === 'ChannelTextAreaContainer', false);
 		patch('pog-blurnsfw', channelTextArea.type, 'render', (args, res) => {
 			const channel = args[0].channel;
-			const blockedChannels =
-				this.settings.get('Blocked') !== undefined
-					? this.settings.get('Blocked').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0])
-					: '';
-			const blurChannels =
-				this.settings.get('Blur') !== undefined
-					? this.settings.get('Blur').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0])
-					: '';
-			if (blockedChannels.length !== 0) return res;
-			else if (blurChannels.length !== 0) this.blurChannel();
-			else if ((this.settings.get('blurInDm') && channel.type === 1) || (this.settings.get('blurInGroup') && channel.type === 3)) this.blurChannel();
+			if ((this.settings.get('blurInDm') && channel.type === 1) || (this.settings.get('blurInGroup') && channel.type === 3)) this.blurChannel();
 			else if (channel.nsfw && channel.type === 0) this.blurChannel();
 			return res;
 		});
